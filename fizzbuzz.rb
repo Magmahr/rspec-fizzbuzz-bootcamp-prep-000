458d071be5be0f49a4ea93647232f5219f5cfd6b
Matt Maher <mattmaher0@gmail.com>
2018-04-29 19:52:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,11 @@
-require 'pry'
 
-def fizzbuzz
-  binding.pry
 
+def fizzbuzz(num)
+  if num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  end
 end
